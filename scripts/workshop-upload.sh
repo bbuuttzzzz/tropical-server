07492d4e8ec0747936d4e9a266f1ff6064598f2b
Drew <pantsrain@gmail.com>
2024-08-07 14:26:32 -0500

    tweaking addon creation script

--- scripts/workshop-upload.sh
+++ scripts/workshop-upload.sh
@@ -2,6 +2,6 @@ GMOD_ROOT="/q/Steam/steamapps/common/GarrysMod"
 
 set -e
 
-"$GMOD_ROOT/bin/gmad.exe" create -folder . zscontent.gma
-"$GMOD_ROOT/bin/gmpublish.exe" update -addon zscontent.gma -id "1479340077"
-rm zscontent.gma
+"$GMOD_ROOT/bin/gmad.exe" create -folder . ".gma"
+"$GMOD_ROOT/bin/gmpublish.exe" update -addon ".gma" -id "1479340077"
+rm .gma
