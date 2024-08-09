507fd8abd39009368ad908f877d740d6c0ffa630
Drew <pantsrain@gmail.com>
2024-08-09 18:05:29 -0500

    fix workshop content file ignoring vtx files

--- scripts/workshop-upload-lua.sh
+++ scripts/workshop-upload-lua.sh
@@ -3,7 +3,6 @@ GMOD_ROOT="/q/Steam/steamapps/common/GarrysMod"
 
 set -e
 
-echo "Building LUA content addon..."
 "$GMOD_ROOT/bin/gmad.exe" create -folder . ".gma"
 "$GMOD_ROOT/bin/gmpublish.exe" update -addon ".gma" -id "1479340077"
 rm .gma
