e2efe744391f3ce9fbc3060653b94a22824301a5
Drew <pantsrain@gmail.com>
2024-08-09 12:32:56 -0500

    split workshop addon into gamemode and content

---
+++ scripts/workshop-upload-content.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+GMOD_ROOT="/q/Steam/steamapps/common/GarrysMod"
+
+set -e
+set -x
+
+rm -rf /tmp/addon
+mkdir /tmp/addon
+
+cp scripts/zs-content-workshop-addon.json /tmp/addon/addon.json
+cp -r gamemodes/zombiesurvival_modded/content/* /tmp/addon/
+cd /tmp/addon
+
+"$GMOD_ROOT/bin/gmad.exe" create -folder . ".gma"
+"$GMOD_ROOT/bin/gmpublish.exe" update -addon ".gma" -id "1479340077"
+rm .gma
