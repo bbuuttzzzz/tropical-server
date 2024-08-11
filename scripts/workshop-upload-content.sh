2a53a694ea59711c992a55e5d528d59b1ff82f48
Drew <pantsrain@gmail.com>
2024-08-10 23:48:42 -0500

    fix wrong gamemode id for workshop-upload-content

--- scripts/workshop-upload-content.sh
+++ scripts/workshop-upload-content.sh
@@ -12,5 +12,5 @@ cp -r gamemodes/zombiesurvival/content/* /tmp/addon/
 cd /tmp/addon
 
 "$GMOD_ROOT/bin/gmad.exe" create -folder . ".gma"
-"$GMOD_ROOT/bin/gmpublish.exe" update -addon ".gma" -id "1479340077"
+"$GMOD_ROOT/bin/gmpublish.exe" update -addon ".gma" -id "3305863875"
 rm .gma
